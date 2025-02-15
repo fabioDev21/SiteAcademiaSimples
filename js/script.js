3928b6e04279a73d9c6a8a501baa11f6aa3c53cd
fabioDev21 <fabiotwist1@gmail.com>
2025-02-14 22:10:09 -0300

    feat provisorio de adicionar novo treino, falta estilizar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+const allContainer = document.querySelector(`.allContainer`)
+window.addEventListener(`load`, () => {
+    const treinosAdicionados = document.querySelector('#treinosAdicionados')
+    const treinosUltimos = document.querySelector('#treinosUltimos')
+    const treinosProntos = document.querySelector('#treinosProntos')
+    const imgPlaceholderSemTreinos = `<img src="../assets/imgs/img-sem-treinos.png">`
+
+    if(!treinosAdicionados.children.length < 1 || !treinosUltimos.children.length < 1 || !treinosProntos.children.length < 1){
+        treinosAdicionados.insertAdjacentHTML(`beforeend`, imgPlaceholderSemTreinos)
+        treinosUltimos.insertAdjacentHTML(`beforeend`, imgPlaceholderSemTreinos)
+        treinosProntos.insertAdjacentHTML(`beforeend`, imgPlaceholderSemTreinos)
+    }
+
+
+})
+
 const formMeusTreinos = document.querySelector('#adicionarNovoFrm')
 formMeusTreinos.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -5,23 +21,49 @@ formMeusTreinos.addEventListener('submit', (e) => {
 })
 
 function criaNovoTreino(){
+    const idElemento = Date.now()
     const nomeDoTreino = document.querySelector('#nomeTreinoInpt').value
     const qntSeries = document.querySelector('#qntSeriesInpt').value
     const parteExercitada = document.querySelector('#parteExercInpt').value
+    const cargaExercicio = document.querySelector('#cargaExercicio').value
+
+
+
+    /* Explicação desta lógica: 
+        Pegamos o primeiro arquivo selecionado (files[0]).  
+        Criamos uma URL temporária com URL.createObjectURL(imagemArquivo),
+        que permite exibir a imagem sem precisar enviá-la para um servidor.
+    */ 
+    const dadoImagemColetado = document.querySelector('#imagemDoTreino').files[0]
+    const imagemDoTreino = URL.createObjectURL(dadoImagemColetado)
 
     const novoTreinoCriado = {
+        idCaixaTreino: idElemento,
         nome: nomeDoTreino,
         series: qntSeries,
-        parte: parteExercitada
+        parte: parteExercitada,
+        carga: cargaExercicio,
+        imgTreino: imagemDoTreino
     }
 
     mostraNovoTreino(novoTreinoCriado)
 }
 
-function mostraNovoTreino({nome, series, parte}){
-    console.log(`O nome do seu novo treino é: ${nome}, com ${series} series necessárias e você deve treinar a ${parte}!`)
+function mostraNovoTreino({idCaixaTreino, nome, series, parte, carga, imgTreino}){
 
-    // corrigir detalhes de amostragem no console, por exemplo a parte precisa mostrar separado e fora que deve-se mostrar o treino na tela da página.
+
+    const caixaParaAddTreinos = document.querySelector("#treinosAdicionados")
+    const novoTreino = `
+        <div id="treino${idCaixaTreino}" class="elementoTreino">
+            <img src="${imgTreino}" alt="Imagem do treino adicionado">
+            <h4>${nome} - ${parte}</h4>
+            <p>Características: </p>
+            <p>Repetições ou Séries: ${series}</p>
+            <p>Carga: ${carga}</p>
+        </div>` 
+    caixaParaAddTreinos.insertAdjacentHTML('afterbegin', novoTreino)
+
+    // estilizar a forma como o treino aparece e como a imagem do treino aparece, estilizar formulario
     // que seja possível editar o treino também,.muito necessário.
     // fazer o crud complete dessa página de treinos e refatorar o código para seu devido depois
     // fazer o condicionamento de parâmetros recebidos!!!
